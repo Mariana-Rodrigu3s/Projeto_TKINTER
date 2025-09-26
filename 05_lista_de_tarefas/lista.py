22d8e9fcd4e694974cd794482d72b44996b35f19
Mariana Rodrigues <mariana.s.rodrigues13@aluno.senai.br>
2025-09-26 09:53:42 -0300

    Fazendo uma ListBox

--- 05_lista_de_tarefas/lista.py
+++ 05_lista_de_tarefas/lista.py
@@ -1,5 +1,6 @@
 import ttkbootstrap as ttk
 from tkinter import messagebox
+from tkinter import Listbox
 
 
 
@@ -8,18 +9,37 @@ class Lista:
 
         self.janela = ttk.Window(title= "Lista de Tarefas", themename= "minty")
         self.janela.geometry("800x600")
-        self.janela.resizable(False,
-                            False)
+        self.janela.resizable(False,False)
         self.janela.iconbitmap("05_lista_de_tarefas/download.ico")
 
+        self.janela = ttk.Label(text= "Lista de Tarefa").pack()
         
         
+        frame = ttk.Frame(self.janela)
+        frame.pack()
+
+        self.tarefa = ttk.Entry(frame)
+        self.tarefa.pack(side="left", expand=True, fill= "x")
+
+        adicionar = ttk.Button(frame, text= "Adicionar Tarefa")
+        adicionar.pack(side="right")
         
-        
+        list = Listbox(self.janela, font=("Segoue UI", 12), height=10)
+        list.pack(pady=20, padx=20, fill="both", expand= True)
 
 
+        frame_botao = ttk.Frame(self.janela)
+        frame_botao.pack(side="bottom")
+
+        excluir = ttk.Button(frame_botao, text="Excluir")
+        excluir.pack(side="right")
+
+        feito = ttk.Button(frame_botao, text="Feito")
+        feito.pack(side="left")
 
 
+    
+    
 
 
 
